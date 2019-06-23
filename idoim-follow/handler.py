c4ac533936108df672b1769da449b563a34c8e49
zengguangtao <zengguangtao@bytedance.com>
2019-06-23 15:56:43 +0800

    init
    Change-Id: I6c206e390e56110e5428bdcaa25817bc2472ba0e

--- idoim-follow/handler.py
+++ idoim-follow/handler.py
@@ -1,23 +1,17 @@
 import json
 import random
 
-def idoim_follow(w):
+def handle(req):
     with open('./idioms_dict.json', 'r') as f:
         words = json.load(f)
     try:
         if w not in words[w[0]]:
-            print('the idiom not exist')
-            return
+            return 'the idiom not exist'
         next_words = words.get(w[-1], [])
         length = len(next_words)
         if length == 0:
-            print("can't find the next idion")
-            return
+            return "can't find the next idion")
         word = next_words[random.randint(0, length - 1)]
-        print(word)
+        return word
     except:
-        print('bad parameter')
-
-if __name__ == "__main__":
-    word = input()
-    idoim_follow(word.strip())
+        return 'bad parameter'
